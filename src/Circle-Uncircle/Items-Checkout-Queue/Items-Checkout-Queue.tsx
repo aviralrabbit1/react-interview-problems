f8f326e7dbc6f40bc2d8a2feb90308ab9d6a5268
Aviral Verma <aviral.robin@gmail.com>
2023-09-19 20:32:50 +0530

    css shap/color changes

--- src/Circle-Uncircle/Items-Checkout-Queue/Items-Checkout-Queue.tsx
+++ src/Circle-Uncircle/Items-Checkout-Queue/Items-Checkout-Queue.tsx
@@ -46,16 +46,57 @@ export default function ItemsCheckoutQueue() {
         flexDirection: "column", // Center vertically
         gap: "20px"
     }}>
+      <h1>
         Items-Checkout-Queue
+      </h1>
         <form onSubmit={addItemsToQueue}>
             <input required type="number" value={items} 
             onChange={(e) => setItems(e.currentTarget.valueAsNumber)} />
             <button>Checkout</button>
         </form>
-        <div className="lines" style={{display:'grid', gridTemplateColumns:'repeat(5, 1fr)', gap: '50px'}}>
-        {lines.map((people, index) => (
-            <div key={index}>Q{index+1}</div>
-            ))}
+        <div className="lines" style={{
+          display:'grid', 
+          gridTemplateColumns:'repeat(5, 1fr)', 
+          gap: '50px',
+          // alignItems: "center",
+          justifyContent: "center",
+          }}>
+        {lines.map((line, index) => (
+            <div key={index}>
+                <h1 style={{paddingBottom:'10px'}} >
+                  Q{index}
+                </h1>
+                <h3 className="rectangle" 
+                style={{
+                  color: 'cyan', 
+                  paddingRight:'10px', 
+                  left:'20%',
+                  width: '100px',
+                  height: '30px',
+                  border: '1px solid white',
+                  padding: '2px',
+                  alignItems: "center",
+                  justifyContent: "center",                  
+                  }}>
+                  Total: {line.reduce((sum, value) => sum+value, 0)}
+                </h3>
+                <h3 style={{
+                  marginTop:'15px', 
+                  color: 'yellow',
+                  }}>
+
+              {line.map(items => (
+                <li style={{
+                  listStyle:'none',                   
+                  width: '30px',
+                  borderRadius: '50%',
+                  border: '1px solid white',
+                  padding:'3px'
+                }} >{items}</li>
+                ))}
+                </h3>
+            </div>
+        ))}
         </div>
     </div> 
   )
