cadbdc73bd3fdedce1ed6f7a2f165626da7504c6
Aviral Verma <aviral.robin@gmail.com>
2023-09-19 16:28:31 +0530

    basic form setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { CircleUncircle } from './Circle-Uncircle/CircleUncircle';
 import Tree from './Folder-Tree-structure-display/Tree';
 
 import './style.css';
+import ItemsCheckoutQueue from './Circle-Uncircle/Items-Checkout-Queue/Items-Checkout-Queue';
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div className='App'>
-      <CircleUncircle />
+      <ItemsCheckoutQueue />
     </div>
   );
 };
---
+++ src/Circle-Uncircle/Items-Checkout-Queue/Items-Checkout-Queue.tsx
@@ -0,0 +1,14 @@
+// Take input of number of items for checkout, 
+//  and push it into a queue from the given number of queus with least items
+
+export default function ItemsCheckoutQueue() {
+  return (
+    <div>Items-Checkout-Queue
+        <form>
+            <input type="text" />
+            <button>Checkout</button>
+        </form>
+
+    </div> 
+  )
+}
