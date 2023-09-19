018f593cc77b020a334793e214874b875853c329
Aviral Verma <aviral.robin@gmail.com>
2023-09-19 16:34:25 +0530

    taking value on change

--- src/Circle-Uncircle/Items-Checkout-Queue/Items-Checkout-Queue.tsx
+++ src/Circle-Uncircle/Items-Checkout-Queue/Items-Checkout-Queue.tsx
@@ -1,14 +1,17 @@
 // Take input of number of items for checkout, 
 //  and push it into a queue from the given number of queus with least items
 
+import { useState } from "react";
+
 export default function ItemsCheckoutQueue() {
+    const [items, setItems] = useState(0);
   return (
     <div>Items-Checkout-Queue
         <form>
-            <input type="text" />
+            <input required type="number" value={items} 
+            onChange={(e) => setItems(e.currentTarget.valueAsNumber)} />
             <button>Checkout</button>
         </form>
-
     </div> 
   )
 }
